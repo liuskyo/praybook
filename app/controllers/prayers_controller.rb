fa052457b19215db204f058a180ad90010ef6a52
劉天佑 <gn00595067@gmail.com>
2016-03-07 17:51:32 +0800

    增加基本發代禱文功能

--- app/controllers/prayers_controller.rb
+++ app/controllers/prayers_controller.rb
@@ -2,14 +2,23 @@ class PrayersController < ApplicationController
 		before_action :authenticate_user!,:except=>[:index]
 
 	def index
-    @prayer=Prayer.all
+    @prayer = Prayer.all
 	end
 
+	def new
+		@prayer = Prayer.new
+	end
 
+	def create
+		@prayer = Prayer.new(prayer_params)
+  	@prayer.user = current_user
 
-
-
-
+  	if @prayer.save
+  		redirect_to prayers_path #tell browser http code:303
+  	else
+  		render prayers_path
+  	end
+	end
 
 
 
